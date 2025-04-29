7a3b1955e8798faa799f5090416a58c4f8e0e259
MridulNikhanj <nikhanjmridul@gmail.com>
2025-04-30 01:25:09 +0530

    Initial commit1

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\CategoryCotroller;
-use App\Http\Controllers\CropController;
-use App\Http\Controllers\FarmController;
-use App\Http\Controllers\farmCropController;
-use App\Http\Controllers\farmRegisterController;
-use App\Http\Controllers\LeaseController;
-use App\Http\Controllers\NoteControlller;
+// use App\Http\Controllers\CategoryCotroller;
+// use App\Http\Controllers\CropController;
+// use App\Http\Controllers\FarmController;
+// use App\Http\Controllers\farmCropController;
+// use App\Http\Controllers\farmRegisterController;
+// use App\Http\Controllers\LeaseController;
+// use App\Http\Controllers\NoteControlller;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -33,13 +33,13 @@ Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::resource('crop', CropController::class);
-Route::resource('category', CategoryCotroller::class);
-Route::resource('farm', FarmController::class);
-Route::resource('note', NoteControlller::class);
-Route::resource('lease', LeaseController::class);
-Route::resource('farm-crop', farmCropController::class);
-Route::resource('register', farmRegisterController::class);
+// Route::resource('crop', CropController::class);
+// Route::resource('category', CategoryCotroller::class);
+// Route::resource('farm', FarmController::class);
+// Route::resource('note', NoteControlller::class);
+// Route::resource('lease', LeaseController::class);
+// Route::resource('farm-crop', farmCropController::class);
+// Route::resource('register', farmRegisterController::class);
 
 Route::group(['middleware'=>['auth']] ,function(){
 
